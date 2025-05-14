cdc7a5e08dc5bcf060c61a740316ec7c9f3a1ed3
chxhyxn <chxhyxn@gmail.com>
2025-05-14 21:48:32 +0900

    Initial Commit

---
+++ TCATODO/TCATODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATODOApp.swift
+//  TCATODO
+//
+//  Created by SeanCho on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TCATODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
